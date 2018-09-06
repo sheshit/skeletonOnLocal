613388424786364baa7cfff3111c36b586586385
sheshit <sheshitkarthikeya237@gmail.com>
2018-09-07 02:50:27 +0530

    google user data loaded to mongo

--- app/screens/signinscreen.js
+++ app/screens/signinscreen.js
@@ -13,22 +13,28 @@ export default class SignInScreen extends React.Component {
     super(props);
     this.state={
       accessToken:"",
+      name:"",
+      email:"",
+      photoUrl:""
     };
   }
 
   accessTokenExport = () => {
     var data = {
       "accessToken": this.state.accessToken,
+      "name": this.state.name,
+      "email":this.state.email,
+      "photoUrl":this.state.photoUrl
     }
     console.log("accessTokenExport called");
-    fetch("http://192.168.201.69:3000/google-login",{
+    fetch("http://192.168.31.115:3000/google-login",{
       method:"POST",
       headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
       },
       body:  JSON.stringify(data)
-    }).then(Alert.alert("Sent access token"))
+    }).then(Alert.alert("Sent data"))
   }
 
   signInWithGoogleAsync = async () => {
@@ -38,13 +44,16 @@ export default class SignInScreen extends React.Component {
           iosClientId: '701891613865-3cjbhhtcfsvj69the1n9vdbanur6hbtj.apps.googleusercontent.com',
           scopes: ['profile', 'email'],
         });
-
+      //  console.log(JSON.stringify(result));
         if (result.type === 'success') {
           this.setState({
             accessToken:result.accessToken,
+            name:result.user.name,
+            email:result.user.email,
+            photoUrl:result.user.photoUrl
           });
           this.accessTokenExport();
-          console.log(this.state.accessToken);
+        //  console.log(this.state.accessToken);
           console.log("Successful");
 
           this.props.navigation.navigate('App');
